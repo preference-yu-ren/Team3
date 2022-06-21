abb7508a8a267567defac57ba3c4e4cbbfb0faba
zhuogezuiniubi <1449128079@qq.com>
2022-06-21 15:20:52 +0800

    更新服务端

--- udp_server.c
+++ udp_server.c
@@ -12,7 +12,7 @@ int main()
 	struct sockaddr_in my_addr,cl_addr;
 	int addrlen = sizeof(cl_addr);
 	char buf[64]={0};
-	char msg[64]={0};
+	char msg[64]="hello client";
 	
 	//1.创建UDP socket
 	sock = socket(AF_INET,SOCK_DGRAM,0);
@@ -22,26 +22,28 @@ int main()
 	return -1;
 	}
 	
-	while(1)
+	//2.绑定IP地址与端口号
+	bzero(&my_addr,sizeof(my_addr));
+	my_addr.sin_family=AF_INET;//地址结构的协议族
+	my_addr.sin_port=htons(MYPORT);//地址结构的端口地址，网络字节序
+	my_addr.sin_addr.s_addr=htonl(INADDR_ANY);//服务器IP地址
+	if (-1 == bind(sock,(struct sockaddr *)&my_addr,sizeof(my_addr)))
 	{
-		
-		//2.绑定IP地址与端口号
-		bzero(&my_addr,sizeof(my_addr));
-		my_addr.sin_family=AF_INET;//地址结构的协议族
-		my_addr.sin_port=htons(MYPORT);//地址结构的端口地址，网络字节序
-		my_addr.sin_addr.s_addr=INADDR_ANY;//服务器IP地址
-		if (-1 == bind(sock,(struct sockaddr *)&my_addr,sizeof(struct sockaddr)))
-		{
 		printf("bind failed\n");
 		return -1;
-		}
+	}
+	
+	while(1)
+	{
+		memset(msg, 0, sizeof(msg));
+		memset(buf, 0, sizeof(buf));
 		//3.接收客户端请求
 		recvfrom(sock,buf,sizeof(buf),0,(struct sockaddr *)&cl_addr,&addrlen);
-		printf("客户端请求:%s",buf);
+		printf("客户端请求:%s\n",buf);
 		
 		//从控制台输入聊天信息
 		printf("输入向客户端返回的消息:\n");
-		//scanf("%s",msg);
+		scanf("%s",msg);
 		
 		//4.发送服务器应答
 		sendto(sock,msg,strlen(msg),0,(struct sockaddr *)&cl_addr,sizeof(cl_addr));
