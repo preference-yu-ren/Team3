6afe8e1ec8d54b1af1e23317eeb32c69a133d67d
zhuogezuiniubi <1449128079@qq.com>
2022-06-21 15:20:35 +0800

    更新客户端

--- udp_client.c
+++ udp_client.c
@@ -15,22 +15,25 @@ int main()
 	char buf[64]={0};
 	//1.创建UDP socket
 	sock = socket (AF_INET,SOCK_DGRAM,0);
-	
+	bzero(&addr,sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port=htons(SERVER_PORT);//服务器的端口
+	addr.sin_addr.s_addr=inet_addr("192.168.0.103");//服务器IP
+	//addr.sin_addr.s_addr=inet_addr("127.0.0.1");//内部循环 服务器IP
+		
 	while(1){
+		
+		memset(msg, 0, sizeof(msg));
+		memset(buf, 0, sizeof(buf));
 		//从控制台输入聊天信息
 		printf("输入向服务器发送的消息:\n");
-		scanf("%s\n",msg);
+		scanf("%s",msg);
 		
 		//2.发送客户端请求
-		bzero(&addr,sizeof(addr));
-		addr.sin_family = AF_INET;
-		addr.sin_port=htons(SERVER_PORT);//服务器的端口
-		addr.sin_addr.s_addr=inet_addr("192.168.71.66");//服务器IP
-		sendto(sock,msg,strlen(msg),0,(struct sockaddr *)&addr,addrlen);
-	
+		sendto(sock,msg,strlen(msg),0,(struct sockaddr *)&addr, addrlen);
 	
 		//3.接收服务器应答
-		recvfrom(sock,buf,sizeof(buf),0,(struct sockaddr *)&addr,&addrlen);
+		recvfrom(sock,buf,sizeof(buf),0,(struct sockaddr *)&addr, &addrlen);
 		printf("服务器应答:%s\n",buf);
 	}
 	
